22d657765f2fddfa13694cbcc02f10b0a615521f
Nahuel <49825382+NahuelStratico@users.noreply.github.com>
2023-06-16 19:31:35 -0300

    started how do we teach

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,18 @@
 import culture from '../assets/culture.png'
 import hablemos from '../assets/hablemos.png'
 import caminito from '../assets/caminitoclases.png'
-import start from '../assets/start.png'
+// import start from '../assets/start.png'
+import comunicative from '../assets/communicative.png'
+import visual from '../assets/visual.png'
+import storytelling from '../assets/storytelling.png'
+import gamification from '../assets/gamification.png'
+import how1 from '../assets/howTeach1.png'
+import how2 from '../assets/howTeach2.png'
+import how3 from '../assets/howTeach3.png'
+import how4 from '../assets/howTeach4.png'
 
 import '../styles/home.css'
+import { NavLink } from 'react-router-dom'
 
 const Home = () => {
 
@@ -12,9 +21,9 @@ const Home = () => {
     <>
     {/* Initial text "Hola mundo" */}
       <div className="container py-5">
-        <div className="row my-5 justify-content-center flex-md-row flex-column align-items-center">
+        <div className="row my-sm-5 justify-content-center flex-md-row flex-column align-items-center">
 
-          <div className="col-6 d-flex flex-column justify-content-center align-items-center icon_container">
+          <div className="col-6 d-none d-sm-flex flex-column justify-content-center align-items-center icon_container">
             <div className='hablemos-icon'>
               <img className='img-fluid flex-end' src={hablemos} alt="hablemos" />
             </div>
@@ -42,20 +51,64 @@ const Home = () => {
 
           <div className='text-center how_teach-container'>
             <h2 className='title_howTeach'>How do we teach?</h2>
-            <p>communicative approach</p>
-            <p>Visual and cognitive grammar</p>
-            <p>Culture exchange </p>
-            <p>gamification</p>
-            <p className='mb-0'>storytelling</p>
-          </div>
-
-          <div className='image_container'>
-            {/* <img src="" alt="" /> */}
-            
+            <div className='how-teach_container'>
+              <div className='icon_container-how-teach'>
+                <img className='img_howteach' src={comunicative} alt="" />
+                <p>communicative approach</p>
+              </div>
+              <div className='icon_container-how-teach'>
+                <img className='img_howteach' src={visual} alt="" />
+                <p>Visual and cognitive grammar</p>
+              </div>
+              <div className='icon_container-how-teach'>
+                <img className='img_howteach' src={gamification} alt="" />
+                <p>gamification</p>
+              </div>
+              <div className='icon_container-how-teach'>
+                <img className='img_howteach' src={storytelling} alt="" />
+                <p>storytelling</p>
+              </div>
+            </div>
           </div>
 
         </div>
       </div>
+
+      <div className='image_container'>
+        <div className='img_content'>
+          <img className="img-fluid" src={how3} alt="" />
+        </div>
+        <div className='img_content'>
+          <img className="img-fluid" src={how2} alt="" />
+        </div>
+        <div className='img_content'>
+          <img className="img-fluid" src={how1} alt="" />
+        </div>
+        <div className='img_content'>
+          <img className="img-fluid" src={how4} alt="" />
+        </div>
+      </div>
+
+    {/* Meet your Teachers */}
+    <div className='container'>
+
+      <div className='teachers_container text-center'>
+        <h3 className='title_meet'>meet your</h3>
+        <h2 className='teachers'>teachers</h2>
+      </div>
+
+      <div className='d-flex flex-column flex-md-row'>
+        <div className='daniela_container'>
+          <NavLink className="link_linkedin" to="/"/>
+          {/* <img className='img_daniela' src={} alt="" /> */}
+        </div>
+        <div className='abril_container'>
+          <NavLink className="link_linkedin" to="/"/>
+          {/* <img className='img_abril' src={} alt="" /> */}
+        </div>
+      </div>
+
+    </div>
 
       <div style={{height:'100vh'}}>
 
