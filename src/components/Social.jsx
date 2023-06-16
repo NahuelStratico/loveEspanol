420a85b1fba3b50819bae96fcf66015b0164efa3
Nahuel <49825382+NahuelStratico@users.noreply.github.com>
2023-06-16 10:37:30 -0300

    started home jsx

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -10,42 +10,43 @@ import '../styles/social.css'
 const Social = () => {
   return (
     <div className='header_social'>
-      <div className='claim'>
-        <a className='link_trial' href="#" target="_blank">
-          <p>Claim your free trial</p>
-        </a>
-      </div>
-
-      <div className='suscribe'>
-        <div className='suscribe-content'>
-          <div className='suscribe-text'>
-            <p>Suscribe for FREE to Snack the Español </p>
-            <img src={iconArrow} className='arrow-icon'/>
-            <p> Podcast and newsletter with exclusive content</p>
-          </div>
-          <div className='suscribe_icons'>
-            <a className='link_trial' href="#" target="_blank">
-              <img className='icons-social letter' src={ iconLetter } alt="icon letter" />
-            </a>
-            <a className='link_trial' href="#" target="_blank">
-              <img className='icons-social micro' src={ iconMicrophone } alt="icon microphone" />
-            </a>
-          </div>
-        </div>
-
-        <div className='icons-social_container'>
+        <div className='claim'>
           <a className='link_trial' href="#" target="_blank">
-            <img className='icons-social' src={ icWtsp } alt="icon whatsapp" />
-          </a>
-          <a className='link_trial' href="#" target="_blank">
-            <img className='icons-social' src={ icSpotify } alt="icon spotify" />
-          </a>
-          <a className='link_trial' href="#" target="_blank">
-            <img className='icons-social' src={ icInstagram } alt="icon instagram" />
+            <p>Claim your free trial</p>
           </a>
         </div>
 
-      </div>
+        <div className='suscribe'>
+          <div className='suscribe-content'>
+            <div className='suscribe-text'>
+              <p>Suscribe for FREE to Snack the Español </p>
+              <img src={iconArrow} className='arrow-icon'/>
+              <p> Podcast and newsletter with exclusive content</p>
+            </div>
+            <div className='suscribe_icons'>
+              <a className='link_trial' href="#" target="_blank">
+                <img className='icons-social letter' src={ iconLetter } alt="icon letter" />
+              </a>
+              <a className='link_trial' href="#" target="_blank">
+                <img className='icons-social micro' src={ iconMicrophone } alt="icon microphone" />
+              </a>
+            </div>
+          </div>
+
+          <div className='icons-social_container'>
+            <a className='link_trial' href="#" target="_blank">
+              <img className='icons-social' src={ icWtsp } alt="icon whatsapp" />
+            </a>
+            <a className='link_trial' href="#" target="_blank">
+              <img className='icons-social' src={ icSpotify } alt="icon spotify" />
+            </a>
+            <a className='link_trial' href="#" target="_blank">
+              <img className='icons-social' src={ icInstagram } alt="icon instagram" />
+            </a>
+          </div>
+
+        </div>
+     
     </div>  
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,66 @@
 
+import culture from '../assets/culture.png'
+import hablemos from '../assets/hablemos.png'
+import caminito from '../assets/caminitoclases.png'
+import start from '../assets/start.png'
+
+import '../styles/home.css'
+
 const Home = () => {
 
   return (
-    <h1>¿Qué es Lorem Ipsum?
-    Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
-    
-    ¿Por qué lo usamos?
-    Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo "Contenido aquí, contenido aquí". Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de "Lorem Ipsum" va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras veces a propósito (por ejemplo insertándole humor y cosas por el estilo).
-    
-    
-    ¿De dónde viene?
-    Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo, haciendo que este adquiera mas de 2000 años de antiguedad. Richard McClintock, un profesor de Latin de la Universidad de Hampden-Sydney en Virginia, encontró una de las palabras más oscuras de la lengua del latín, "consecteur", en un pasaje de Lorem Ipsum, y al seguir leyendo distintos textos del latín, descubrió la fuente indudable. Lorem Ipsum viene de las secciones 1.10.32 y 1.10.33 de "de Finnibus Bonorum et Malorum" (Los Extremos del Bien y El Mal) por Cicero, escrito en el año 45 antes de Cristo. Este libro es un tratado de teoría de éticas, muy popular durante el Renacimiento. La primera linea del Lorem Ipsum, "Lorem ipsum dolor sit amet..", viene de una linea en la sección 1.10.32</h1>
+    <>
+    {/* Initial text "Hola mundo" */}
+      <div className="container py-5">
+        <div className="row my-5 justify-content-center flex-md-row flex-column align-items-center">
+
+          <div className="col-6 d-flex flex-column justify-content-center align-items-center icon_container">
+            <div className='hablemos-icon'>
+              <img className='img-fluid flex-end' src={hablemos} alt="hablemos" />
+            </div>
+            <div className='culture-icon'>
+              <img className='img-fluid' src={culture} alt="culture" />
+            </div>
+          </div>
+
+          <div className="col-6 d-flex flex-column justify-content-center align-items-start initial_text-container">
+            <h3 className='hola_mundo'>hola mundo</h3>
+            <p className='paragraph'>Somos Love Español...</p>
+            <p className='paragraph'>A young and dynamic online Language School based in Argentina but you can take online lessons with us no matter where you are. We offer a Spanish learning experience like no other. ¿Vamos?</p>
+          </div>
+
+        </div>
+      </div>
+
+      <div className='step_img h-6'>
+        <img className='img-fluid' src={caminito} alt="camito clases" />
+      </div>
+
+    {/* How do we Teach */}
+      <div className='how_container py-5'>
+        <div className='container-fluid'>
+
+          <div className='text-center how_teach-container'>
+            <h2 className='title_howTeach'>How do we teach?</h2>
+            <p>communicative approach</p>
+            <p>Visual and cognitive grammar</p>
+            <p>Culture exchange </p>
+            <p>gamification</p>
+            <p className='mb-0'>storytelling</p>
+          </div>
+
+          <div className='image_container'>
+            {/* <img src="" alt="" /> */}
+            
+          </div>
+
+        </div>
+      </div>
+
+      <div style={{height:'100vh'}}>
+
+      </div>
+    </>
   )
 }
 
